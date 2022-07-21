4fe26d72dc9860e4a71b38ebdd1b9e789b1f8b0d
Matt <matthew.hope396@outlook.com>
2022-07-21 09:59:10 +0100

    Minor fix

--- src/Zion.EntityFrameworkCore.Projections/DbContexts/ProjectionStateDbContext.cs
+++ src/Zion.EntityFrameworkCore.Projections/DbContexts/ProjectionStateDbContext.cs
@@ -1,23 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Zion.EntityFrameworkCore.Projections.Entities;
-using Zion.EntityFrameworkCore.Projections.EntityTypeConfigurations;
 
 namespace Zion.EntityFrameworkCore.Projections.DbContexts
 {
-    public class ProjectionStateDbContext : DbContext
+    public class ProjectionStateDbContext : ProjectionStateDbContextBase<ProjectionStateDbContext>
     {
-        public DbSet<ProjectionState> ProjectionStates { get; set; }
-
-        protected ProjectionStateDbContext(DbContextOptions options)
-            : base(options)
+        protected ProjectionStateDbContext(DbContextOptions<ProjectionStateDbContext> options) : base(options)
         {
-        }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.ApplyConfiguration(new ProjectionStateEntityTypeConfiguration());
-
-            base.OnModelCreating(builder);
         }
     }
 }
