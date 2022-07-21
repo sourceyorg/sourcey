952bc78951254a200ce19fe348658c8abfa51ed5
Matt <matthew.hope396@outlook.com>
2022-07-21 17:12:46 +0100

    Minor tweak

--- src/Zion.EntityFrameworkCore.Projections/Builder/ZionEntityFrameworkCoreProjection.cs
+++ src/Zion.EntityFrameworkCore.Projections/Builder/ZionEntityFrameworkCoreProjection.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Zion.Core.Extensions;
-using Zion.EntityFrameworkCore.Projections.Configuration;
+﻿using Zion.EntityFrameworkCore.Projections.Configuration;
 using Zion.Projections;
 
 namespace Zion.EntityFrameworkCore.Projections.Builder
@@ -8,18 +6,7 @@ namespace Zion.EntityFrameworkCore.Projections.Builder
     internal class ZionEntityFrameworkCoreProjection<TProjection> : IZionEntityFrameworkCoreProjection<TProjection>
         where TProjection : class, IProjection
     {
-        private readonly IServiceCollection _services;
-
         private int? _interval;
-        private bool _hasManagerSet;
-
-        public ZionEntityFrameworkCoreProjection(IServiceCollection services)
-        {
-            if (services is null)
-                throw new ArgumentNullException(nameof(services));
-
-            _services = services;
-        }
 
         public IZionEntityFrameworkCoreProjection<TProjection> WithInterval(int interval)
         {
@@ -32,11 +19,5 @@ namespace Zion.EntityFrameworkCore.Projections.Builder
             {
                 o.Interval = _interval ?? StoreProjectorOptions<TProjection>.Default.Interval;
             };
-
-        internal void Validate()
-        {
-            if(!_hasManagerSet)
-                throw new InvalidOperationException($"You must set a ProjectionStateDbContext for {typeof(TProjection).FriendlyFullName()}");
-        }
     }
 }
