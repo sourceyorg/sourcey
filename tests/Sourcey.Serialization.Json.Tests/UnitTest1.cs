8cb372cf16b36061f2dc84cb102d6d8174023453
Matt Hope <matthew.hope396@outlook.com>
2023-08-29 17:38:23 +0000

    WIP

---
+++ tests/Sourcey.Serialization.Json.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Sourcey.Serialization.Json.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Sourcey.Tests/Aggregates/AggregateSpecification.cs
@@ -0,0 +1,8 @@
+using System.Reflection.Metadata;
+
+namespace Sourcey.Tests.Aggregates;
+
+public class AggregateSpecification<TResult> : TypeSpecification<TResult>
+{
+
+}
